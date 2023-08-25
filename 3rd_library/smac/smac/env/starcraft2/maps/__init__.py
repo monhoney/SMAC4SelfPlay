cb0bfc43a07691942b6652d0d30a8e686ffef1ea
hskAlena <hyunseo.kim159@gmail.com>
2023-08-25 14:55:16 +0900

    SMACexp map add

--- 3rd_library/smac/smac/env/starcraft2/maps/__init__.py
+++ 3rd_library/smac/smac/env/starcraft2/maps/__init__.py
@@ -2,9 +2,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from smac.env.starcraft2.maps import smac_maps
+from smac.env.starcraft2.maps import smac_maps, smac_plus_maps
 
+def get_map_params(map_name, type="smac_plus"):
+    if type == "smac_plus":
+        map_param_registry = smac_plus_maps.get_smac_map_registry()    
+    elif type == "smac":
+        map_param_registry = smac_maps.get_smac_map_registry()
+    else:
+        NotImplementedError
+    return map_param_registry[map_name]
 
-def get_map_params(map_name):
+def get_map_params_prev(map_name):
     map_param_registry = smac_maps.get_smac_map_registry()
     return map_param_registry[map_name]
