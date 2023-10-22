5a0acd16557d73e395f3a946bc7e3767741f0fe9
hskAlena <hyunseo.kim159@gmail.com>
2023-10-22 19:13:02 +0900

    hs sh

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-python train.py --player_algo SAC --enemy_algo COMPUTER --wandb --seed 0 \
+python train.py --player_algo SAC --enemy_algo BLIZZARD --wandb --seed 0 \
  --epochs 400 --alpha 0.05 --tau 1.0 --hard_update \
- --run_name gaussian-alpha0.05tau1-hardupdate
+ --run_name gaussian-alpha0.05tau1-hardupdate-blizzard
